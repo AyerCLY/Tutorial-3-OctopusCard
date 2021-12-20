cda9d6d0190b1c3bdd53abb9369529f68ae8ccc8
AyerCLY <95411578+AyerCLY@users.noreply.github.com>
2021-12-20 16:51:21 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,51 +1,42 @@
-function OctopusCard () {
+function OctopusCard(){
   this.balance = 0;
   this.aavs_amount = 0;
 
-  // returns boolean
-  // ture: success to set AAVS
-  // false: fail to set AAVS
-  this.setAAVS = function (_aavs_amount) {
-    if (_aavs_amount != 250 || _aavs_amount != 500) return false;
+  this.setAAVS = function(_aavs_amount) {
+    if (_aavs_amount != 250 && _aavs_amount !=500) return false;
     this.aavs_amount = _aavs_amount;
     return true;
   }
 
-  // returns boolean
-  // ture: success to topup
-  // false: fail to topup
-  this.topup = function (_amount) {
-    if (_amount < 0 || _amount % 50 != 0) return false;
-    this.balance += _amount;
+  this.topup = function (_amount){
+    if (_amount < 50 || _amount%50 != 0) return false;
+    this.balance +=_amount;
     return true;
   }
 
-  // returns boolean
-  // ture: success to pay
-  // false: fail to pay
-  this.pay = function (_amount) {
-    if (_amount < 0 || _amount > 3000) return false;
-
-    if (_amount > this.balance) {
-      if (this.aavs_amount == 0){
+  this.pay = function(_fee){
+    if (_fee < 0 || _fee > 3000 ) return false;
+    if (_fee > this.balance) {
+      if (this.aavs_amount ==0){
         return false;
-      } else {
+      }else{
         this.topup(this.aavs_amount);
-      }
+       }
+      if (_fee > this.balance) return false;
     }
-
-    if (_amount > this.balance) return false;
-
-    this.balance -= _amount;
+    this.balance -= _fee;
     return true;
-  }
+  } 
+
 }
 
-var card = new OctopusCard();
-if (card.setAAVS(250)) {
-  console.log('success');
-} else {
-  console.log('fail');
-}
-// card.topup(500);
-// card.pay(500);
+var card = new OctopusCard ()
+console.log(card)
+card.setAAVS(250)
+console.log(card)
+card.topup(100)
+console.log(card)
+card.pay(50)
+console.log(card)
+card.pay(300)
+console.log(card)
